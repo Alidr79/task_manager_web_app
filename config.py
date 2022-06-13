09c2a9506cf4aad76bf99926d3b365f03cd8a403
Alidr79 <ali.derakhshesh79@gmail.com>
2022-06-13 15:03:32 +0430

    added some comments

--- config.py
+++ config.py
@@ -6,6 +6,9 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or "NEISNF$*#(424FS75J"
     # In the previous line we used or to have a default value beside the os.environ
+    # We will never share the secret key so this must be taken from .env file
+    # during the production phase!
+    # but for ease of use in development phase we have set a default key...
 
     @staticmethod
     def init_app(app):
@@ -26,7 +29,7 @@ class TestingConfig(Config):
 
 class ProductionConfig(Config):
     SQL_DATABASE_URL = os.environ.get('DATABASE_URL')
-    # pass
+    # This should be driven from the database path on the pythonanywhere sever...
 
 
 config = {
